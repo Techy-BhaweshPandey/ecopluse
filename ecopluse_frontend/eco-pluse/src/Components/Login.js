9a40503374ff2fd3e8b75fe009c3054450d03366
Techy-BhaweshPandey <bhaweshpandey2004@gmail.com>
2026-04-12 18:06:07 +0530

    Update login API endpoint to production URL

--- ecopluse_frontend/eco-pluse/src/Components/Login.js
+++ ecopluse_frontend/eco-pluse/src/Components/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
   const navigate = useNavigate();
 
   const loginUser = async () => {
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch("https://ecopluse-je8y.onrender.com/api/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
