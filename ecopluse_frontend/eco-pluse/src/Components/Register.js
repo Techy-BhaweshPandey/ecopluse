6d5d8465d37afd78dca1e3c7daa5f67b8f026cbc
Techy-BhaweshPandey <bhaweshpandey2004@gmail.com>
2026-04-12 18:06:30 +0530

    Update registration API endpoint to production URL

--- ecopluse_frontend/eco-pluse/src/Components/Register.js
+++ ecopluse_frontend/eco-pluse/src/Components/Register.js
@@ -17,7 +17,7 @@ const Register = () => {
 
   const handleRegister = async () => {
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch("https://ecopluse-je8y.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
